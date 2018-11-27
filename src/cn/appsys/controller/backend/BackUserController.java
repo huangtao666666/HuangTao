91688741400b8c2fb6439ce7a126c6796ffe0366
huangtao <huang@qq.com>
2018-11-27 10:28:23 +0800

    AppInfoSys-添加了一个测试的jsp文件

--- src/cn/appsys/controller/backend/BackUserController.java
+++ src/cn/appsys/controller/backend/BackUserController.java
@@ -3,10 +3,10 @@ package cn.appsys.controller.backend;
 import cn.appsys.pojo.BackendUser;
 import cn.appsys.service.backend.BackendUserService;
 import cn.appsys.tools.Constants;
+import jdk.nashorn.internal.ir.IfNode;
 import org.apache.log4j.Logger;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.annotation.Resource;
@@ -14,49 +14,45 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
 @Controller
-@RequestMapping(value="/manager")
+@RequestMapping("/guanli")
 public class BackUserController {
     private Logger logger = Logger.getLogger(BackUserController.class);
-
     @Resource
     private BackendUserService backendUserService;
 
-    @RequestMapping(value="/login")
-    public String login(){
-        logger.debug("LoginController welcome AppInfoSystem backend==================");
-        return "backendlogin";
+    @RequestMapping("/login")
+     String login(){
+        logger.debug("======================");
+       return "backendlogin";
     }
 
-    @RequestMapping(value="/dologin",method= RequestMethod.POST)
-    public String doLogin(@RequestParam String userCode,@RequestParam String userPassword,HttpServletRequest request,HttpSession session){
-        logger.debug("doLogin====================================");
-        //调用service方法，进行用户匹配
+    @RequestMapping("dologin")
+    String dologin(@RequestParam String userCode, @RequestParam String userPassword, HttpServletRequest request, HttpSession session){
+        logger.debug("===================================");
         BackendUser user = null;
         try {
-            user = backendUserService.login(userCode,userPassword);
-        } catch (Exception e) {
-            // TODO Auto-generated catch block
+            user=backendUserService.login(userCode,userPassword);
+        }catch (Exception e){
             e.printStackTrace();
         }
-        if(null != user){//登录成功
-            //放入session
-            session.setAttribute(Constants.USER_SESSION, user);
-            //页面跳转（main.jsp）
-            return "redirect:/manager/backend/main";
-        }else{
+        if (null != user) {
+            session.setAttribute(Constants.USER_SESSION,user);
+            return "redirect:/guanli/backend/main";
+        }else {
             //页面跳转（login.jsp）带出提示信息--转发
             request.setAttribute("error", "用户名或密码不正确");
             return "backendlogin";
         }
     }
 
-    @RequestMapping(value="/backend/main")
-    public String main(HttpSession session){
-        if(session.getAttribute(Constants.USER_SESSION) == null){
-            return "redirect:/manager/login";
+    @RequestMapping("backend/main")
+    String main(HttpSession session){
+        if(session.getAttribute(Constants.USER_SESSION)==null){
+            return "redirect:/guanli/login";
         }
         return "backend/main";
     }
+
 
     @RequestMapping(value="/logout")
     public String logout(HttpSession session){
